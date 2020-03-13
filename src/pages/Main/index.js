a2cb7962247e6774cead1e10a6d8591b2757d37e
RennanD <rplaykl@gmail.com>
2020-03-13 00:19:59 -0300

    feat: Create Card

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {
+  Container,
+  Content,
+  CardHeader,
+  Card,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation,
+} from './styles';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
@@ -9,6 +21,25 @@ export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 4,000,00</Description>
+          </CardContent>
+
+          <CardFooter>
+            <Annotation>Tranferência de R$ 1.000,00 recebida ontem</Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
