93e25726988646b67e1f8e7cf5e226afef41e35c
RennanD <rplaykl@gmail.com>
2020-03-12 23:58:22 -0300

    feat: Create Tabs

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
-import { View } from 'react-native';
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, TabsContainer, TabItem, TabText } from './styles';
 
 export default function Tabs() {
-  return <View />;
+  return (
+    <Container>
+      <TabsContainer>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#fff" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+
+        <TabItem>
+          <Icon name="chat-bubble-outline" size={24} color="#fff" />
+          <TabText>Cobrar</TabText>
+        </TabItem>
+
+        <TabItem>
+          <Icon name="arrow-downward" size={24} color="#fff" />
+          <TabText>Depositar</TabText>
+        </TabItem>
+
+        <TabItem>
+          <Icon name="arrow-upward" size={24} color="#fff" />
+          <TabText>Transfrir</TabText>
+        </TabItem>
+
+        <TabItem>
+          <Icon name="lock" size={24} color="#fff" />
+          <TabText>Bloquear cartão</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
+  );
 }
